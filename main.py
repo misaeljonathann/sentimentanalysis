c4ae1bff0dcaffbed35c1c578c68452d0013ccf0
Misael Jonathan <misaeljonathan17@gmail.com>
2018-12-06 18:36:24 +0700

    added tweet crawler, user timeline crawler

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import StreamListener
 from tweepy import Stream
+from pprint import pprint
 import json
+import tweepy
 
 CONSUMER_KEY = "SnxpqkOeT0kNkVC6KRNZPb0fY"
 CONSUMER_KEY_SECRET = "SN1Ld5PjarO3nJG3hSbemsxHPlKsy0DCi3osSCEM9Q0WHmcjUB"
@@ -10,27 +12,44 @@ CONSUMER_KEY_SECRET = "SN1Ld5PjarO3nJG3hSbemsxHPlKsy0DCi3osSCEM9Q0WHmcjUB"
 ACCESS_TOKEN = "1481426107-ChPBgQwWUPZeTiAp0R5XwvMiDKp0JqasHv6yHH5"
 ACCESS_TOKEN_SECRET = "q6E2XhGFYYBQGusbKL1L0SBms1sX9FlA2iEnbDvxEyNrU"
 
-class StdOutListener(StreamListener):
-    def __init__(self):
-        self.counter = 0
-        self.limit = 10
-    def on_data(self, data):
-        with open('data.json', 'w') as outfile:
-            json.dump(data, outfile)
-        if self.counter == self.limit :
-            return False
-        self.counter = self.counter + 1
-        return True
+# class StdOutListener(StreamListener):
+#     def __init__(self):
+#         self.counter = 0
+#         self.limit = 10
+
+#     def on_data(self, data):
+#         json_data = json.dumps(self.data)
+#         val = json.dumps(data, indent=4)
+#         decode = json.loads(val)
+
+#         with open('data.json', 'w') as outfile:
+#             json.dump(data, outfile)
+#         if self.counter == self.limit :
+#             return False
+#         self.counter = self.counter + 1
+        
+#         return True
     
-    def on_error(self, status):
-        print(status)
+#     def on_error(self, status):
+#         print(status)
 
 if __name__ == "__main__":
-    listener = StdOutListener()
+    # listener = StdOutListener()
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    stream = Stream(auth, listener)
+    api = tweepy.API(auth)
+    track = "atta halilintar OR jokowi OR jokowidodo OR prabowo"
+    # search = tweepy.Cursor(api.search, q=track, result_type="recent").items(500)
+    
+    with open('susipudjiastuti.txt', 'w') as outfile:
+        # for item in search:
+        #     outfile.write(str(item.created_at) + " " + str((item.text).encode("utf-8")) + "\n")
+        
+        for status in tweepy.Cursor(api.user_timeline, screen_name="@susipudjiastuti", count=10000, tweet_mode="extended").items():
+            outfile.write(str(status.created_at) + " => " + str((status.full_text).encode("utf-8")) + "\n")
 
-    stream.filter(track=['jokowi dodo', 'jokowi', 'jkw', 'jokowidodo'])
+
+    # stream = Stream(auth, listener)
+    # stream.filter(track=['jokowi dodo', 'jokowi', 'jkw', 'jokowidodo'])
 
--- print.py
+++ print.py
@@ -2,8 +2,8 @@ import json
 
 obj = None
 with open('data.json') as f:
-    print(f)
     obj = json.load(f)
+    print(obj["created_at"])
 #print(json.dumps(parsed, indent=4, sort_keys=True))
 outfile = open('file.json', 'w')
 outfile.write(json.dumps(obj, indent=4, sort_keys=True))
