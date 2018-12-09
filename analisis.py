5a4b59e6238474308dd82cf1f45d78092e33f085
Misael Jonathan <misaeljonathan17@gmail.com>
2018-12-09 18:35:27 +0700

    update

--- analisis.py
+++ analisis.py
@@ -1,18 +1,26 @@
+# import Sastrawi package
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# stem
+
 sentimen = []
+
 with open('tweets.txt', 'r') as file:
     counter = 0
+
     for line in file :
-        # counter+=1
-        # if (counter == 21) :
-        #     break
+        line = stemmer.stem(line)
+        print(line)
         nilai_sentimen = 0
         tweet = line.split(" ")
         with open('lexicon.txt', 'r') as lexicon:
             for lex in lexicon :
                 for kata in tweet :
-                    # print("check antara " + kata.lower() + " <==> " + lex.split(" ")[0])
                     if kata.lower() == lex.split(" ")[0] :
-                        print("DAPET COY")
                         nilai_sentimen += int(lex.split(" ")[1])
         sentimen.append(nilai_sentimen)
 
--- main.py
+++ main.py
@@ -12,47 +12,26 @@ CONSUMER_KEY_SECRET = "SN1Ld5PjarO3nJG3hSbemsxHPlKsy0DCi3osSCEM9Q0WHmcjUB"
 ACCESS_TOKEN = "1481426107-ChPBgQwWUPZeTiAp0R5XwvMiDKp0JqasHv6yHH5"
 ACCESS_TOKEN_SECRET = "q6E2XhGFYYBQGusbKL1L0SBms1sX9FlA2iEnbDvxEyNrU"
 
-# class StdOutListener(StreamListener):
-#     def __init__(self):
-#         self.counter = 0
-#         self.limit = 10
-
-#     def on_data(self, data):
-#         json_data = json.dumps(self.data)
-#         val = json.dumps(data, indent=4)
-#         decode = json.loads(val)
-
-#         with open('data.json', 'w') as outfile:
-#             json.dump(data, outfile)
-#         if self.counter == self.limit :
-#             return False
-#         self.counter = self.counter + 1
-        
-#         return True
-    
-#     def on_error(self, status):
-#         print(status)
-
 if __name__ == "__main__":
-    # listener = StdOutListener()
+
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth)
     track = "jokowi OR jkw OR jokowidodo"
-    search = tweepy.Cursor(api.search, q=track, tweet_mode="extended").items(20)
+    search = tweepy.Cursor(api.search, q=track, tweet_mode="extended").items(3200)
     
     with open('tweets.txt', 'w') as outfile:
-        
+
         # TWEET EXTRACT
-        for item in search:
-            outfile.write(str(item.created_at) + " " + str((item.full_text).encode("ascii", errors='ignore')) + "\n")
+        for tweet_info in search:
+            if 'retweeted_status' in dir(tweet_info):
+                outfile.write(str(tweet_info.created_at) + " " + str((tweet_info.retweeted_status.full_text).encode("ascii", errors='ignore')) + "\n")
+            else:
+                outfile.write(str(tweet_info.created_at) + " " + str((tweet_info.full_text).encode("ascii", errors='ignore')) + "\n")    
         
         # USER EXTRACT
         # for status in tweepy.Cursor(api.user_timeline, screen_name="@susipudjiastuti", count=10000, tweet_mode="extended").items():
         #     outfile.write(str(status.created_at) + " => " + str((status.full_text).encode("utf-8")) + "\n")
 
-
-    # stream = Stream(auth, listener)
-    # stream.filter(track=['jokowi dodo', 'jokowi', 'jkw', 'jokowidodo'])
 
