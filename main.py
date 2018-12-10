4dc4da85218665bddb86b2447983927f5c139ecb
Misael Jonathan <misaeljonathan17@gmail.com>
2018-12-10 12:19:06 +0700

    push

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ from pprint import pprint
 import json
 import tweepy
 
-CONSUMER_KEY = "rg3GQNIK3uPviihUX9zR3ovbn"
-CONSUMER_KEY_SECRET = "2BKF3652v6U4oU2mFnwtjx8zSSIpHyYvfMv4knG0xEStpgFNLT"
+# CONSUMER_KEY = "UUCxBb6SVN4uENYuXOVCigNYg"
+# CONSUMER_KEY_SECRET = "QA7YMhVUJ7x5muQt0SW4Pts9sg4L1TZVLbMELaG7eAO94F6EtN"
 
-ACCESS_TOKEN = "1481426107-DYQFSEE5gNlRHG7fA1x1FgzVHQ3ZrPT1seFb23S"
-ACCESS_TOKEN_SECRET = "RlbPOqAm5V8u0DD93l2qsizvFg05WLbyZqycPGFgYxbEz"
+# ACCESS_TOKEN = "890682047601098753-x1kSBNLehFMO2ornerKnYlX8EHMwvLj"
+# ACCESS_TOKEN_SECRET = "M6AHh6iceDZFZIipVd53dY6T17bT0BBNeeP3lapoW0XIU"
+
+CONSUMER_KEY = "tWnsD74A2Jj3jd8lUqD1uB8tP"
+CONSUMER_KEY_SECRET = "wr3VFrbF9TYTbGdaevjUi9uS6mVJw01mJOshibRQoLFXC0jdmy"
+
+ACCESS_TOKEN = "1071757219417313281-vmfqBRKnGcQWQU2cg74fmeZPHTiOLs"
+ACCESS_TOKEN_SECRET = "UVwotAKVsXweStTrWiObix3vcH2xY5msWWSQcSAeGXGDa"
 
 if __name__ == "__main__":
 
@@ -18,10 +24,11 @@ if __name__ == "__main__":
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth)
-    track = "jokowi OR jkw OR jokowidodo OR"
-    search = tweepy.Cursor(api.search, q=track, tweet_mode="extended").items(3200)
-
-    with open('tweets.txt', 'w') as outfile:
+    # track = "jokowi OR jkw OR jokowidodo"
+    track = "prabowo"
+    search = tweepy.Cursor(api.search, q=track, tweet_mode="extended").items(3000)
+    
+    with open('tweets_prab.txt', 'a') as outfile:
 
         # TWEET EXTRACT
         for tweet_info in search:
@@ -30,4 +37,7 @@ if __name__ == "__main__":
             else:
                 outfile.write(str(tweet_info.created_at) + " " + str((tweet_info.full_text).encode("ascii", errors='ignore')) + "\n")    
         
+        # USER EXTRACT
+        # for status in tweepy.Cursor(api.user_timeline, screen_name="@susipudjiastuti", count=10000, tweet_mode="extended").items():
+        #     outfile.write(str(status.created_at) + " => " + str((status.full_text).encode("utf-8")) + "\n")
 
